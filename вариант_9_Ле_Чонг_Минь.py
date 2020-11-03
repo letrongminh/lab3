9639779264dcfd97dfdf5c54ca195e97f8eb3711
Ле Чонг Минь <59441846+letrongminh@users.noreply.github.com>
2020-11-03 13:39:05 +0300

    update

--- вариант_9_Ле_Чонг_Минь.py
+++ вариант_9_Ле_Чонг_Минь.py
@@ -2,24 +2,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 
-t = np.linspace(0, 4*np.pi, 100000)
+t = np.linspace(0, 4 * np.pi, 100000)
 x1 = np.cos(t)
-x2 = np.cos(6.5*t)
+x2 = np.cos(6.5 * t)
 
 y1 = np.sin(t)
-y2 = np.sin(6.5*t)
+y2 = np.sin(6.5 * t)
 
+x = 13 * (x1 - x2 / 6.5)
+y = 13 * (y1 - y2 / 6.5)
 
-x = 13*(x1 - x2/6.5)
-y = 13*(y1 - y2/6.5)
-
-fig = plt.figure(figsize=(10, 10))
+fig = plt.figure(figsize=(5, 5))
 plt.plot(x, y)
 
 plt.savefig("img_1.png")
 
-img1 = Image.open("img_1.png")
-img1 = img1.convert("1")
-img1 = img1.save("img_result.bmp")
+img1 = Image.open("img_1.png").save("img_result.bmp")
+# img1 = img1.convert("1")
+# img1 = img1.save("img_result.bmp")
 
 plt.show()
